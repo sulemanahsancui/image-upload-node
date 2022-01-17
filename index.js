7fd9580db3ebd525a76f40b7a4b66aec9b08e37b
sulemanahsancui <sulemanahsan12345@gmail.com>
2022-01-17 06:58:10 +0500

    image uploading using express

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const users = require("./routes/user");
+
+//intialize express server instamce
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(fileUpload());
+app.use("/api/v1/users", users);
+// connect to the mongodb
+
+mongoose.connect("mongodb://localhost/user_db", (err, db) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("connection build successfully");
+  }
+});
+
+//listen on port
+
+app.listen(4000, () => {
+  console.log("server running at port 4000");
+});
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getUser,
+  getUsers,
+  createUser,
+  deleteUser,
+  userProfileUploadImage,
+} = require("../controller/user");
+
+router.route("/").get(getUsers).post(createUser);
+router.route("/:id").get(getUser).delete(deleteUser);
+router.route("/profile/:id").post(userProfileUploadImage);
+
+module.exports = router;
